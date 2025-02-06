10d2e3c7e238262bc4cf55dc841506516565faa0
Peter Kincses <kinncses.p@googlemail.com>
2025-02-06 08:49:25 +0000

    Add additional questions for and ts typing

--- data/consultationQuestions.ts
+++ data/consultationQuestions.ts
@@ -7,14 +7,32 @@ export type Question = {
 }
 
 export const questions: Question[] = [
-    { id: "q1", category: "blood-pressure", question: "Are you aged between 18-65?", correctAnswer: true, },
-    { id: "q2", category: "blood-pressure", question: "Are you currently taking any medication?", correctAnswer: false, },
-    { id: "q3", category: "blood-pressure", question: "Do you experience migraines for more than 10 days a month?", correctAnswer: false, },
-    { id: "q4", category: "blood-pressure", question: "Do your migraines last less than 4 hours without treatment or last longer than 24 hours?", correctAnswer: true, },
+    { id: "q1", category: "migraine", question: "Are you aged between 18-65?", correctAnswer: true, },
+    { id: "q2", category: "migraine", question: "Are you currently taking any medication?", correctAnswer: false, },
+    { id: "q3", category: "migraine", question: "Do you experience migraines for more than 10 days a month?", correctAnswer: false, },
+    { id: "q4", category: "migraine", question: "Do your migraines last less than 4 hours without treatment or last longer than 24 hours?", correctAnswer: true, },
     {
-        id: "q5", category: "blood-pressure", question: "Are you experiencing any of the following along with your migraine?", correctAnswer: false,
+        id: "q5", category: "migraine", question: "Are you experiencing any of the following along with your migraine?", correctAnswer: false,
         supplementaryText: {
             html: "<ul><li>Weakness</li><li>Numbness</li><li>Difficulty speaking</li><li>Difficulty walking</li></ul>"
         }
     },
+
+    { id: "q6", category: "hay-fever", question: "Have you previously experienced an allergic reaction to antihistamines such as fexofenadine, loratadine or cetirizine or are you lactose intolerant?", correctAnswer: false, },
+    { id: "q7", category: "hay-fever", question: "Are you breast feeding or pregnant or planning to become pregnant in the next 6 months?", correctAnswer: false, },
+    { id: "q8", category: "hay-fever", question: "Have you been diagnosed with any of the following?", correctAnswer: false, 
+        supplementaryText: {
+            html: "<ul><li>Kidney or liver problems</li><li>A history of heart problems such as an irregular or fast heartbeat or angina</li><li>Epilepsy</li></ul>"
+        }
+    },
+    { id: "q9", category: "hay-fever", question: "Have you taken antihistamine tablets before?", correctAnswer: true, },
+    {
+        id: "q10", category: "hay-fever", question: "Do you understand there is a possibility that oral antihistamines may cause drowsiness and affect driving or operating machinery? You should check that the antihistamine tablets you receive do not make you feel sleepy or dizzy before driving or operating machinery.", 
+        correctAnswer: true,
+    },
+    {
+        id: "q11", category: "hay-fever", question: "Do you understand you should stop taking antihistamine tablets and speak to your GP if your symptoms do not improve after 2 weeks of starting treatment?",
+        correctAnswer: true,
+    },
+    
 ];
--- data/consultationTypes.ts
+++ data/consultationTypes.ts
@@ -1,30 +1,47 @@
-export const ConsultationTypes = [
+type Consultation = {
+    name: string;
+    slug: string;
+    canConsult: boolean;
+}
+export const ConsultationTypes: Consultation[] = [
     {
-        "name": "General Consultation",
-        "slug": "general-consultation",
+        name: "General Consultation",
+        slug: "general-consultation",
+        canConsult: false
     },
     {
-        "name": "Blood Pressure",
-        "slug": "blood-pressure",
+        name: "Blood Pressure",
+        slug: "blood-pressure",
+        canConsult: false
     },
     {
-        "name": "Diabetes",
-        "slug": "diabetes",
+        name: "Diabetes",
+        slug: "diabetes",
+        canConsult: false
     },
     {
-        "name": "Hair Loss",
-        "slug": "hair-loss",
+        name: "Hair Loss",
+        slug: "hair-loss",
+        canConsult: false
     },
     {
-        "name": "High Cholesterol",
-        "slug": "high-cholesterol",
+        name: "Hay Fever",
+        slug: "hay-fever",
+        canConsult: true
     },
     {
-        "name": "Migraine",
-        "slug": "migraine",
+        name: "High Cholesterol",
+        slug: "high-cholesterol",
+        canConsult: false
     },
     {
-        "name": "Weight Loss",
-        "slug": "weight-loss",
+        name: "Migraine",
+        slug: "migraine",
+        canConsult: true
+    },
+    {
+        name: "Weight Loss",
+        slug: "weight-loss",
+        canConsult: false
     },
 ]
